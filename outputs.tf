1b04ae5cae3eb35a1a7b8d41f28759d1998135d5
vot5kor <vot5kor@gmail.com>
2025-02-24 18:59:06 +0530

    vpc module added

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,16 @@
 /* output "subnets_info"{
     value = aws_subnet.public[0].id
 } */
+
+output "vpc_id"{
+    value = aws_vpc.main.id
+}
+output "public_subnets"{
+    value = aws_subnet.public[*].id
+}
+output "private_subnets"{
+    value = aws_subnet.private[*].id
+}
+output "database_subnets"{
+    value = aws_subnet.database[*].id
+}
