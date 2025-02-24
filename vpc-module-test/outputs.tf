d101753e9bdbea01728f31af99a88d0d55c2458d
vot5kor <vot5kor@gmail.com>
2025-02-24 16:53:26 +0530

    vpc module added

--- vpc-module-test/outputs.tf
+++
@@ -1,7 +0,0 @@
-/* output "subnet_info"{
-    value = module.vpc.subnet_info.names
-} */
-
-/* output "subnets_info" {
-    value = module.vpc.subnets_info
-} */
--- vpc-module-test/parameter.tf
+++
@@ -1,5 +0,0 @@
-/* resource "aws_ssm_parameter" "vpc_id" {
-  name  = "/${var.project_name}/${var.environment}/vpc_id"
-  type  = "String"
-  value = module.vpc.vpc_id
-} */
